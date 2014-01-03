94958c6969f2fa75d883ae277abb1bd965e128e6
jescriba <escribirajoshua@gmail.com>
2014-01-03 15:34:30 -0800

    Methods take hashes and bumped version.

--- lib/cios_helpers.rb
+++ lib/cios_helpers.rb
@@ -3,27 +3,36 @@ require "calabash-cucumber/ibase"
 
 module CiosHelpers
 
-  def until_element_exists element, action, timeout = 30
-    '''Performs a lambda action until the element appear.'''
-    wait_poll until_exists: element, timeout: timeout do
+  def until_element_exists opts = {}
+    '''Performs a lambda action until the element appears.
+    The default action is to do nothing.'''
+    raise "No element given." if opts[:element].nil?
+    timeout = opts[:timeout] || 10
+    action = opts[:action] || lambda { ; }
+    wait_poll until_exists: opts[:element], timeout: timeout do
       action.call
     end
   end
 
-  def once_element_exists element, action = lambda { touch element }, timeout = 30
+  def once_element_exists opts = {}
     '''Performs a lambda action once the element exists.
     The default behavior is to touch the specified element.'''
-    wait_for_elements_exist [element], timeout: timeout
+    raise "No element given." if opts[:element].nil?
+    timeout = opts[:timeout] || 10
+    action = opts[:action] || lambda { touch opts[:element] }
+    wait_for_elements_exist [opts[:element]], timeout: timeout
     action.call
   end
 
-  def multiple_traits
+  def multiple_traits opts = {}
     '''Pass an array of query elements. Determines the
-    correct trait for page objects that can have multiple
+    correct trait for page objects that can have multiple 
     acceptable traits.'''
+    timeout = opts[:timeout] || 10
+    traits = opts[:traits] || ["*"]
     trait = ''
     action = lambda do 
-      arr.each do |element|
+      traits.each do |element|
         if element_exists element
           trait = element
           break
